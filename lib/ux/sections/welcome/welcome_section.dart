fdc607e41b225b0ba68093cd08583831bb2196d2
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2019-08-23 10:57:32 +0200

    Add waves.

--- lib/ux/sections/welcome/welcome_section.dart
+++ lib/ux/sections/welcome/welcome_section.dart
@@ -1,29 +1,51 @@
 import 'package:flutter_web/material.dart';
+import 'package:jfkdev/theme.dart';
 import 'package:jfkdev/ux/sections/welcome/introduction_header.dart';
 import 'package:jfkdev/ux/widgets/fill_screen.dart';
+import 'package:jfkdev/ux/widgets/wave/config.dart';
+import 'package:jfkdev/ux/widgets/wave/wave.dart';
 import 'package:jfkdev/ux/widgets/widget_utils.dart';
 
 class WelcomeSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FillScreen(
-      child: Stack(
-        alignment: Alignment.center,
+      child: Column(
         children: <Widget>[
-          Column(
-            children: <Widget>[
-              verticalMargin36,
-              Flexible(
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 64.0),
-                    child: IntroductionHeader(),
-                  ),
-                ),
+          verticalMargin32,
+          FittedBox(
+            fit: BoxFit.contain,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 64.0),
+              child: IntroductionHeader(),
+            ),
+          ),
+          Expanded(child: emptyWidget),
+          Flexible(
+            child: WaveWidget(
+              config: WaveConfigCustom(
+                gradients: [
+                  [AppTheme.colorBackgroundLight, Colors.blue],
+                  [Colors.blue[800], Colors.blue[300]],
+                  [Colors.blue[900], AppTheme.colorBackgroundLight],
+                  [Colors.cyan, AppTheme.colorGreen],
+                ],
+                durations: [
+                  35000,
+                  19440,
+                  10800,
+                  6000,
+                ],
+                heightPercentages: [
+                  0.20,
+                  0.23,
+                  0.25,
+                  0.30,
+                ],
+                gradientBegin: Alignment.bottomLeft,
+                gradientEnd: Alignment.topRight,
               ),
-              const Spacer(flex: 2),
-            ],
+            ),
           ),
         ],
       ),
---
+++ lib/ux/widgets/wave/config.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_web/widgets.dart';
+
+abstract class WaveConfig {
+  const WaveConfig();
+}
+
+class WaveConfigCustom extends WaveConfig {
+  const WaveConfigCustom({
+    this.colors,
+    this.gradients,
+    this.gradientBegin,
+    this.gradientEnd,
+    @required this.durations,
+    @required this.heightPercentages,
+    this.blur,
+  })  : assert(colors != null || gradients != null, '`color or `gradients` must be provided.'),
+        assert(colors == null || gradients == null, 'Cannot provide both colors and gradients.'),
+        assert(durations != null, '`durations` must be provided.'),
+        assert(heightPercentages != null, '`heightPercentages` must be provided.'),
+        assert(
+          colors == null ||
+              colors != null && colors.length == durations.length && colors.length == heightPercentages.length,
+          'Length of `colors`, `durations` and `heightPercentages` must be equal.',
+        );
+
+  final List<Color> colors;
+  final List<List<Color>> gradients;
+  final Alignment gradientBegin;
+  final Alignment gradientEnd;
+  final List<int> durations;
+  final List<double> heightPercentages;
+  final MaskFilter blur;
+}
+
+// TODO: Implement.
+class RandomConfig extends WaveConfig {
+  RandomConfig();
+}
+
+// TODO: Implement.
+class SingleConfig extends WaveConfig {
+  SingleConfig();
+}
