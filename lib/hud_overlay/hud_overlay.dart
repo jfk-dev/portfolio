6a26db47551d3373fb1e5c51113c1de13a6227e7
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2022-05-23 16:05:16 +0200

    feat: add google fonts, improve hud structure

--- lib/hud_overlay/hud_overlay.dart
+++ lib/hud_overlay/hud_overlay.dart
@@ -1,4 +1,7 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:portfolio/theme/theme.dart';
 import 'package:provider/provider.dart';
 
 class HudOverlay extends StatelessWidget {
@@ -6,24 +9,97 @@ class HudOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const AnimatedLogo();
+  }
+}
+
+class AnimatedLogo extends StatelessWidget {
+  const AnimatedLogo({super.key});
+
+  /// The offset indicating the top left of the screen.
+  static const topLeft = Offset.zero;
+
+  /// At what percentage of one full page scroll the logo should be placed at
+  /// the top left of the screen.
+  ///
+  /// For example, a value of `0.5` means that if the user scrolls half a page,
+  /// the logo will be placed at the top left of the screen.
+  static const logoCenteredOffsetPercentage = 0.5;
+
+  /// The width of the center logo.
+  static const logoWidth = 100.0;
+
+  /// The height of the center logo.
+  static const logoHeight = 100.0;
+
+  Offset lerpOffset(
+    Offset begin,
+    Offset end,
+    double t, {
+    Curve curve = Curves.linear,
+  }) {
+    var effectiveCurve = curve;
+    if (begin > end) {
+      effectiveCurve = effectiveCurve.flipped;
+    }
+    final minX = math.min(begin.dx, end.dx);
+    final maxX = math.max(begin.dx, end.dx);
+    final interpolatedX = begin.dx + (end.dx - begin.dx) * t;
+
+    /// The X-component normalized to be between 0.0 and 1.0.
+    final normalizedX = (interpolatedX - minX) / (maxX - minX);
+    final curvedX = effectiveCurve.transform(normalizedX) * (maxX - minX);
+
+    final minY = math.min(begin.dy, end.dy);
+    final maxY = math.max(begin.dy, end.dy);
+    final interpolatedY = begin.dy + (end.dy - begin.dy) * t;
+
+    /// The Y-component normalized to be between 0.0 and 1.0.
+    final normalizedY = (interpolatedY - minY) / (maxY - minY);
+    final curvedY = effectiveCurve.transform(normalizedY) * (maxY - minY);
+
+    return Offset(
+      curvedX,
+      curvedY,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     final controller = context.watch<ScrollController>();
     final offset = controller.offset;
-    final size = MediaQuery.of(context).size;
-    final fullPagesScrolled = controller.offset / size.height;
 
-    return Center(
-      child: Text(
-        {
-          'Offset': offset,
-          'Full Pages Scrolled': fullPagesScrolled,
-        }.entries.fold<String>(
-              '',
-              (acc, cur) => '$acc\n'
-                  '${cur.key}: '
-                  '${cur.value.toStringAsFixed(2).padLeft(6, '0')}',
-            ),
-        style: const TextStyle(
-          color: Colors.white,
+    final fullPagesScrolled = controller.offset / size.height;
+    final offsetT =
+        (fullPagesScrolled / logoCenteredOffsetPercentage).clamp(0.0, 1.0);
+
+    final screenCenter = size.center(Offset.zero);
+    final logoCenter = Offset(
+      screenCenter.dx - (logoWidth / 2),
+      screenCenter.dy - (logoHeight / 2),
+    );
+
+    final logoOffset = lerpOffset(
+      logoCenter,
+      topLeft,
+      offsetT,
+      curve: Curves.easeInOut,
+    );
+
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 1000),
+      curve: Curves.elasticOut,
+      left: logoOffset.dx,
+      top: logoOffset.dy,
+      child: Container(
+        width: logoWidth,
+        height: logoHeight,
+        padding: const EdgeInsets.all(8),
+        child: FlutterLogo(
+          style: FlutterLogoStyle.stacked,
+          textColor: Theme.of(context).customColors.pink,
         ),
       ),
     );
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/theme/theme.dart';
 
 export 'colors.dart';
 
 abstract class PortfolioTheme {
   static ThemeData light() {
-    return ThemeData.light().copyWith(
+    final base = ThemeData.light();
+    return base.copyWith(
       extensions: const [
         CustomColors.light(),
       ],
+      textTheme: GoogleFonts.firaSansTextTheme(base.textTheme),
     );
   }
 
   static ThemeData dark() {
-    return ThemeData.dark().copyWith(
+    final base = ThemeData.dark();
+    return base.copyWith(
       extensions: const [
         CustomColors.dark(),
       ],
+      textTheme: GoogleFonts.firaSansTextTheme(base.textTheme),
     );
   }
 }
